dea72dc79611ffe81834ddfe91210f7bbcc3b7de
RJOSchool <24010228-n8i@risshisha.onmicrosoft.com>
2025-07-04 10:18:29 +0900

    janken 0.1

---
+++ janken.py
@@ -0,0 +1,13 @@
+import random
+
+def opponent(x):
+    x : int = random.randint(1,3)
+
+    return x
+
+user : input = input("rock, paper, scissor?")
+
+choice : dict = {"rock":1,
+                 "paper":2
+                 "scissor":3}
+
